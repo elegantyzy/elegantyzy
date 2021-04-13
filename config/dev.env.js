146639a31456175b13abba48d02b78df1e479e76
翩翩少年郎 <1258291382@qq.com>
2021-04-13 14:34:20 +0800

    first commit

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  BASE_API: '"http://118.123.245.79:58026"'
+  // BASE_API: '"http://localhost:8080"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://admin-api.macrozheng.com"'
+  // BASE_API: '"http://admin-api.macrozheng.com"'
+  BASE_API: '"http://118.123.245.79:58026"'
 }
